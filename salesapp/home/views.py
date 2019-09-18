cc4bd2d1f36679bf16b7965ab7bc82b623ad3740
Jun Yoshida <yoshida@dynamo-tech.com>
2019-09-18 23:04:34 +0700

    Add top page

--- salesapp/home/views.py
+++ salesapp/home/views.py
@@ -1,3 +1,7 @@
+from django.middleware.csrf import get_token
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    csrf_token = get_token(request)
+    return render(request, 'home/index.html')
