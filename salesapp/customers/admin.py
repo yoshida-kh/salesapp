e6907605facd00e43a15124f01f7a2bf081c7624
Jun Yoshida <yoshida@dynamo-tech.com>
2019-09-24 20:51:28 +0700

    Add models in customer application

--- salesapp/customers/admin.py
+++ salesapp/customers/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Client, Person, Status, Sale
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email", "tel", "note"]
+    search_fields = ["name", "email", "tel"]
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email", "tel", "note"]
+    search_fields = ["name", "email", "tel"]
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "note"]
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "client", "date", "status", "note"]
+    search_fields = ["name", "client", "status"]
--- salesapp/customers/models.py
+++ salesapp/customers/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampBaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class DetailBaseModel(models.Model):
+    name = models.CharField(max_length=500, db_index=True)
+    note = models.CharField(max_length=1000)
+
+    class Meta:
+        abstract = True
+
+
+class Client(TimestampBaseModel, DetailBaseModel):
+    address = models.CharField(max_length=1000)
+    email = models.EmailField(max_length=250)
+    tel = models.CharField(max_length=50)
+
+
+class Person(TimestampBaseModel, DetailBaseModel):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=250)
+    tel = models.CharField(max_length=50)
+
+
+class Status(TimestampBaseModel, DetailBaseModel):
+    pass
+
+
+class Sale(TimestampBaseModel, DetailBaseModel):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    date = models.DateField(null=True)
+    amount = models.FloatField(null=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
