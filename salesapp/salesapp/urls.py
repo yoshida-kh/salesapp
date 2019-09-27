161b541bcc5658069c79a4b8654da6f9d1fcbb05
Jun Yoshida <yoshida@dynamo-tech.com>
2019-09-27 22:00:15 +0700

    Add ModelAdmin into admin_site.register arguments

--- salesapp/salesapp/urls.py
+++ salesapp/salesapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
-from customers.admin import MyAdminSite
+from customers.admin import MyAdminSite, ClientAdmin, PersonAdmin, StatusAdmin, SaleAdmin
 from customers.models import Client, Person, Status, Sale
 
 admin_site = MyAdminSite()
 admin_site.site_title = 'Sales Application'
 admin_site.site_header = 'Sales Application'
 admin_site.index_title = 'Menu'
-admin_site.register([Client, Person, Status, Sale])
+admin_site.register(Client, ClientAdmin)
+admin_site.register(Person, PersonAdmin)
+admin_site.register(Status, StatusAdmin)
+admin_site.register(Sale, SaleAdmin)
 
 urlpatterns = [
     path('', include('home.urls')),
